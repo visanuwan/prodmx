f3590d191f3bfbc93510dd9383d2c69090ba6d49
Visanu Wanchai <visanuw86@gmail.com>
2020-03-16 23:00:22 -0500

    fixed setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,27 @@
+import os
+import sys
 from setuptools import setup
 
+if sys.version_info[:2] < (3,5):
+      raise RuntimeError("Python version >=3.5 required.")
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname), 'r') as handle:
+        data = handle.read()
+    return data
+
 setup(name='prodmx',
-      version='beta',
+      version='Beta',
+      install_requires=['pandas>=0.25.1', 'h5py>=2.9.0', 'numpy>=1.17.2', 'tqdm>=4.36.1', 'scipy>=1.3.1'],
       description='Protein Functional Domain Analysis based on Compressed Sparse Matrix',
+      long_description=read('README.md'),
       url='https://github.com/visanuwan/prodmx',
+      classifiers= [
+            'Development Status :: 4 - Beta',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: MIT License',
+            'Topic :: Scientific/Engineering :: Bio-Informatics'
+      ],
       author='Visanu Wanchai',
       author_email='visanuw86@gmail.com',
       license='MIT',
